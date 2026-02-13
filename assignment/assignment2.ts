15a5a4ec180015d591ff9db799a7677426c53111
Rahul Rao <rahul.rao@fattmerchant.com>
2026-02-13 18:53:56 +0530

    assignment 2 completed

---
+++ assignment/assignment2.ts
@@ -0,0 +1,59 @@
+interface customerDetails {    // creating interface to define the data type on each key value pair inside the object
+    customerName:string,
+    creditScore:number,
+    income:number,
+    isEmployed : boolean,
+    deptToIncomeRatio : number
+}
+
+let customerData1:customerDetails={    // creating object and assigning the type "customerDetails"
+    customerName:"B RAHUL RAO",
+    creditScore:651,
+    income:51000,
+    isEmployed : true,
+    deptToIncomeRatio : 400.0
+}
+
+let customerData2:customerDetails={    // creating object and assigning the type "customerDetails"
+    customerName:"John doe",
+    creditScore:651,
+    income:51000,
+    isEmployed : true,
+    deptToIncomeRatio : 4.0
+}
+
+
+
+function checkLoanEligibility(customer:customerDetails){        //creating a function called checkLoanEligibility and cretaing a variable customer to pass the argument//we can pass the object given above 
+    if(customer.creditScore > 750){
+        console.log(`${customer.customerName},Your loan is approved.`)
+    }
+    else if(customer.creditScore < 650){
+        console.log(`${customer.customerName},Your loan is denied`)
+    }
+
+    else if (customer.creditScore >= 650 && customer.creditScore <=750){
+        console.log("Doing additonal Check ");
+        if(customer.income <= 50000)
+            {
+            console.log(`${customer.customerName}Loan denied because salary is less than 50000`)
+            }
+        else
+             {
+            console.log(`Income is ${customer.income}, which is greater than the minimum requirement, Now checking if the customer is employed`)
+            if (!customer.isEmployed)
+                {
+                console.log(`${customer.customerName}, Loan denied as you are unemployed`)
+                }
+            else if(customer.deptToIncomeRatio < 40.0 )
+             {
+        console.log(`${customer.customerName} is employed and DTI is less than 40 so loan pass`)
+             }
+            else{
+                console.log(`${customer.customerName} LOAN DENIED, DTI is more than 40`)
+            }
+        }
+        }
+}
+checkLoanEligibility(customerData1)    //calling the function by passing the object as argument
+checkLoanEligibility(customerData2)    //calling the function by passing the object as argument
