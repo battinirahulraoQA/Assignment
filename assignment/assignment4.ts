477c52cddb3d89402675060c9a03cc946782d3fd
Rahul Rao <rahul.rao@fattmerchant.com>
2026-02-15 02:09:20 +0530

    assignment 4  completed

--- assignment/assignment4.ts
+++ assignment/assignment4.ts
@@ -19,3 +19,27 @@ Loops and conditional statements
 Transaction with Amount” and also print total number of suspicious transactions */ 
 
 let transactionAmount:number[] =[50000,-2000,3000,-15000,-200,-300,4000,-3000]
+let creditTotal:number=0
+let debitTotal:number=0
+let creditCount=0
+let debitCount=0
+let susAmount =0
+let balance =0
+for(let i:number=0; i<transactionAmount.length ;i++){
+    balance+=transactionAmount[i]
+if(transactionAmount[i]>0){
+    creditCount++
+    creditTotal+=transactionAmount[i]
+}else{
+    debitCount++
+    debitTotal+=transactionAmount[i]
+}
+if(transactionAmount[i]>10000 || transactionAmount[i]<-10000){
+    susAmount++
+}
+}
+console.log(`1) Total number of credit: ${creditCount} , Total number of debits: ${debitCount}`)
+console.log(`2) Total amount credited: ${creditTotal}`)
+console.log(`3) Total amount debited: ${Math.abs(debitTotal)}`) //Math.abs(number)
+console.log(`4) Total amount remaining at the end in Bank Account ${balance}`)
+console.log(`5) Total number Suspicious credit/ debit Transaction with Amount ${susAmount}`)
